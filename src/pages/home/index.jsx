f44442bc58ad24ede89b409338d60ff433b0f97a
Thomgomes <math.011@hotmail.com>
2024-09-14 15:49:40 -0300

    Home centrailzada

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,24 +1,30 @@
-import Layout from '../../components/layout'
-import styles from './index.module.css'
+import Button from "../../components/Buttons/Button";
+import ButtonForm from "../../components/Buttons/ButtonForm";
+import Layout from "../../components/layout";
+import styles from "./index.module.css";
 
 export default function Home() {
   return (
     <Layout>
-        <div className={styles.ok}>
-          <div>
-            <h2>Escaneie objetos com o Visua</h2>
-          </div>
-          <p>
+      <section className={styles.home}>
+        <div className={styles.content}>
+          <h2 className={styles.title}>Escaneie objetos com o Visua</h2>
+          <p className={styles.text}>
             Visua é uma aplicação para pessoas com deficiência visual, onde você
             pode escanear objetos ao seu redor ou escanear o código de barras
             para assim receber descrições detalhadas em áudio e em texto,
             permitindo uma experiência mais inclusiva e independente.
           </p>
-          <p>
+          <p className={styles.text}>
             Explore o mundo ao seu redor com confiança, graças à tecnologia do
             Visua, tornando a informação visual acessível à todos.
           </p>
+          <div className={styles.buttonDiv}>
+            <ButtonForm>Criar Conta</ButtonForm>
+            <Button>Saiba Mais</Button>
+          </div>
         </div>
+      </section>
     </Layout>
-  )
+  );
 }
